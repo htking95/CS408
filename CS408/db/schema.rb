89776ab25b846841ec5cfb6d050f6313e0eb4449
cruz30 <cruz30@purdue.edu>
2016-02-12 02:44:18 -0500

    Removed unnecessary fields in reviews database

--- CS408/db/schema.rb
+++ CS408/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160212053237) do
+ActiveRecord::Schema.define(version: 20160212073015) do
 
   create_table "average_caches", force: :cascade do |t|
     t.integer  "rater_id"
@@ -82,8 +82,6 @@ ActiveRecord::Schema.define(version: 20160212053237) do
   add_index "rating_caches", ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type"
 
   create_table "reviews", force: :cascade do |t|
-    t.integer  "UserID"
-    t.integer  "CourseID"
     t.text     "Comment"
     t.integer  "HelpfulCount"
     t.integer  "Unhelpful"
@@ -101,6 +99,7 @@ ActiveRecord::Schema.define(version: 20160212053237) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.integer  "course_id"
   end
 
   create_table "users", force: :cascade do |t|
