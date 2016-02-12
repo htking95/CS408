637cffcb93d3c70405a5ea6be4c9fef8f02e03b9
cruz30 <cruz30@purdue.edu>
2016-02-12 02:10:06 -0500

    Users are linked to reviews and alerts have been styled

--- CS408/db/schema.rb
+++ CS408/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160212030440) do
+ActiveRecord::Schema.define(version: 20160212053237) do
 
   create_table "average_caches", force: :cascade do |t|
     t.integer  "rater_id"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 20160212030440) do
     t.integer  "DepartmentID"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
