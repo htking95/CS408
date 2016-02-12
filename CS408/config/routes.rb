a239e5eb05f23050cad64c72cc2cdb34138c741e
cruz30 <cruz30@purdue.edu>
2016-02-12 03:40:11 -0500

    Linked reviews with courses

--- CS408/config/routes.rb
+++ CS408/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :reviews, except: [:show, :index]
   resources :courses do
+    resources :reviews, except: [:show, :index]
     collection do
       get 'search'
     end
