e3974b90e8f349660d02c93ecb5264f030d54554
nschumacher <nschuma@purdue.edu>
2016-03-03 09:06:28 -0800

    small changes to searching

--- CS408/app/models/course.rb
+++ CS408/app/models/course.rb
@@ -1,12 +1,15 @@
 class Course < ActiveRecord::Base
 	has_many :reviews
 
-	searchkick word_start: [:courseNum, :name, :department]
+	searchkick word_start: [:courseNum, :name, :department, :fullName]
+	
+	# this tells searchkick what data to index for searching
 	def search_data
 		{
 			courseNum: courseNum,
 			name: name,
-			department: department
+			department: department,
+			fullName: fullName
 		}
 	end
 
