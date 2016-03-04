f1197af74da62b86b715a0e8f1c0950c4b45ebe2
Michelle Young <ymichelle57@yahoo.com>
2016-03-03 20:16:53 -0500

    Updated database

--- CS408/db/migrate/20160219023043_add_abbreviation_again.rb
+++
@@ -1,5 +0,0 @@
-class AddAbbreviationAgain < ActiveRecord::Migration
-  def change
-  	add_column :departments, :abbreviation, :string 
-  end
-end
--- CS408/db/schema.rb
+++ CS408/db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228220533) do
+ActiveRecord::Schema.define(version: 20160303024203) do
+
   create_table "answers", force: :cascade do |t|
     t.text     "answer"
     t.integer  "rating"
@@ -49,10 +50,10 @@ ActiveRecord::Schema.define(version: 20160228220533) do
   end
 
   create_table "departments", force: :cascade do |t|
+    t.string   "abbreviation"
     t.string   "name"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.string   "abbreviation"
   end
 
   create_table "instructors", force: :cascade do |t|
@@ -145,5 +146,20 @@ ActiveRecord::Schema.define(version: 20160228220533) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "votable_id"
+    t.string   "votable_type"
+    t.integer  "voter_id"
+    t.string   "voter_type"
+    t.boolean  "vote_flag"
+    t.string   "vote_scope"
+    t.integer  "vote_weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
 
 end
