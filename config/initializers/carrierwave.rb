be05b4f38f3f8a7d0feb0da93e68f9751b2bd272
cruz30 <cruz30@purdue.edu>
2016-03-04 20:16:55 -0500

    Secured S3 access keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAINCFZ6S4IRSNGEGA',       # required
-    :aws_secret_access_key  => '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', #required                       # required
+    :provider               => 'AWS',                          # required
+    :aws_access_key_id      => ENV["aws_access_key_id"],       # required
+    :aws_secret_access_key  => ENV["aws_secret_access_key"],   # required
   }
-  config.fog_directory  = 'ratemyclasses'            # required
+  config.fog_directory  = ENV['fog_directory']       # required
   config.fog_public     = false                      # optional, defaults to true
   config.fog_authenticated_url_expiration = 600      # (in seconds) => 10 minutes
 end
